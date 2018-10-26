a139ab8b0d358dff3e3bfbf1e0c1177ae3cb35ef
BT_1N3_20@BKTECH.LOCAL <BT_1N3_20@BKTECH.LOCAL>
2018-10-26 09:31:40 -0400

    Initial commit

---
+++ src/JackOLantern.java
@@ -0,0 +1,14 @@
+public class JackOLantern{
+    public void edit (java.lang.String replace, int row, int column)
+    {
+
+    }
+    public java.lang.String toString()
+    {
+
+    }
+    public void fill(java.lang.String str)
+    {
+        
+    }
+}
