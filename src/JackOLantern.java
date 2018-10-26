f7a3ef217b956ed9d4b0403ad183063bc1319d34
BT_1N3_20 <tlee3526@bths.edu>
2018-10-26 10:17:21 -0400

    Hi

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,14 +1,25 @@
-public class JackOLantern{
+public class JackOLantern extends java.lang.Object{
+    String [][] faceFeatures;
+    JackOlantern(java.lang.String[][] faceFeatures)
+    {
+        String faceFeatures =
+    }
     public void edit (java.lang.String replace, int row, int column)
     {
 
     }
     public java.lang.String toString()
     {
-
+        for ( int i = 0; i < faceFeatures.length; i++)
+        {
+            for ( int j = 0; j < faceFeatures [i].length; j++)
+            {
+                System.out.println(faceFeatures [i][j]);
+            }
+        }
     }
     public void fill(java.lang.String str)
     {
-        
+
     }
 }
